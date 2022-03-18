832fc5116907b52a05c0426ff1fc956123c893b6
abeljohnjacob <96794577+abeljohnjacob@users.noreply.github.com>
2022-03-18 16:52:11 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void create();
+int main()
+{
+	create();
+	return 0;
+}
+void create()
+{
+	int arr[100],n,i=0;
+	printf("Enter the limit of the array: ");
+	scanf("%d",&n);
+	printf("Enter array elements: ",n);
+	while(i<=n-1)
+	{
+		scanf("%d",&arr[i]);
+		i++;
+	}
+	printf("Entered array elements are: ");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",arr[i]);
+	}
+	return 0;
+}
